0eab974e7dcdbe458d61393278e8d50799ebbcf3
bsnpcbe <Leo-Y.Liu@aia.com>
2020-06-08 18:33:37 +0800

    first commit
    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react';
+import _ from 'lodash'
+// import ReactParticleLine from 'react-particle-line';
+// import animate from 'animate.css';
+import Header from '@/pages/Header';
+// import Equipment from '@/pages/Equipment';
+// import Map from '@/pages/Map';
+// import Loan from '@/pages/Loan';
+// import Customer from '@/pages/Customer';
+// import Product from '@/pages/Product';
+// import Trading from '@/pages/Trading';
+
+import styles from './index.scss';
+
+export default class index extends PureComponent {
+  constructor(p) {
+    super(p)
+    this.state={
+      scale: this.getScale()
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.setScale)
+  }
+
+  getScale=() => {
+    const {width=1920, height=538.5} = this.props
+    const ww=window.innerWidth/width
+    const wh=window.innerHeight/height
+    return ww<wh?ww: wh
+  }
+
+  setScale = _.debounce(() => {
+    const scale=this.getScale()
+    // eslint-disable-next-line react/no-unused-state
+    this.setState({ scale })
+  }, 500)
+  
+  render() {
+    const {width=1920, height=1080} = this.props
+    const {scale} = this.state
+    return (
+      <div
+        className={styles.homeWrap}
+        style={{
+        transform: `scale(${scale}) translate(0, 0)`,
+        WebkitTransform: `scale(${scale}) translate(0, 0)`,
+        width,
+        height,
+      }}
+      >
+        <div className={styles.header}>
+          <Header />
+        </div>
+        {/* <div className={styles.topLeft}>
+          <Equipment />
+        </div>
+        <div className={`${styles.topCenter} ${animate.animated} ${animate.zoomIn}`}>
+          <Map />
+        </div>
+        <div className={styles.topRight}>
+          <Loan />
+        </div>
+        <div className={styles.bottomLeft}>
+          <Customer />
+        </div>
+        <div className={styles.bottomCenter}>
+          <Product />
+        </div>
+        <div className={styles.bottomRight}>
+          <Trading />
+        </div> */}
+      </div>
+    );
+  }
+}
